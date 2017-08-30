dcbf16e05cbed3aceb5c15ab81e708b3515c730b
timreimherr <timreimherr@gmail.com>
2017-08-30 15:38:30 -0500

    creates directory in Documents and writes database file in it

--- learningWindowsForms/learningWindowsForms/DAL/Repositories/BaseRepository.cs
+++ learningWindowsForms/learningWindowsForms/DAL/Repositories/BaseRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Data.SQLite;
 
 namespace learningWindowsForms.DAL.Repositories
@@ -7,11 +8,12 @@ namespace learningWindowsForms.DAL.Repositories
     {
         public static string DbFile
         {
-            get { return Environment.CurrentDirectory + "\\SimpleDb.sqlite"; }
+            get { return System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),"WebServiceUI", "Database.sqlite"); }
         }
 
         public static SQLiteConnection SimpleDbConnection()
         {
+            System.IO.Directory.CreateDirectory(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "WebServiceUI"));
             return new SQLiteConnection("Data Source=" + DbFile);
         }
 
